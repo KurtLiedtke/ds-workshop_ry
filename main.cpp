a85cad5e9862e645bcc27ab46325610e9ce5f1d0
battistary <ryanskier18@icloud.com>
2024-10-04 14:27:07 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <list>
+
+int main(int argc, char* argv[]) {
+    std::list<std::string> list;
+    for (int i = 1; i < argc; i++) {
+        list.push_back(argv[i]);
+    }
+
+    for (std::list<std::string>::iterator itr = list.begin(); itr != list.end(); itr++) {
+        std::cout << *itr << '\n';
+    }
+
+    return 0;
+}
